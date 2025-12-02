a6c5c336f20ae1d04ccc63b5c223afa0db9c3a1e
heebalz006-cpu <heebalz006@gmail.com>
2025-12-02 01:30:54 +0100

    interface 3 is ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import OFPPTApp from './interface';
+import Filieres from './interface3';
 import EducationLevel from './interface2';
 
 function App() {
   return (
     <div className="App">
-      <EducationLevel />
+      <OFPPTApp />
     </div>
   );
 }
